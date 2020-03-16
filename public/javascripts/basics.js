e0bcc511c5f7f5a5e20a5c4c52dc8a4a1f618054
curmet <ntjust26@gmail.com>
2020-03-16 18:56:35 +0900

    Changed style for search bar

--- public/javascripts/basics.js
+++ public/javascripts/basics.js
@@ -78,22 +78,22 @@ $( document ).ready(function() {
   });
 
 
-  $(".menu-button").on('click', expand);
-
-  var lastScrollTop = 0;
-  // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-  window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-  var st = window.pageYOffset || document.documentElement.scrollTop;// Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-  if (st > lastScrollTop && $(".active").length == 0){
-    // $(".site-title").addClass("menu-win-scroll");
-
-        $(".rating-selector").addClass("rating-select-scroll");
-  } else {
-    // $(".site-title").removeClass("menu-win-scroll");
-        $(".rating-selector").removeClass("rating-select-scroll");
-  }
-  lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-}, false);
+//   $(".menu-button").on('click', expand);
+//
+//   var lastScrollTop = 0;
+//   // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+//   window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+//   var st = window.pageYOffset || document.documentElement.scrollTop;// Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+//   if (st > lastScrollTop && $(".active").length == 0){
+//     // $(".site-title").addClass("menu-win-scroll");
+//
+//         // $(".rating-selector").addClass("rating-select-scroll");
+//   } else {
+//     // $(".site-title").removeClass("menu-win-scroll");
+//         // $(".rating-selector").removeClass("rating-select-scroll");
+//   }
+//   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+// }, false);
 
 ///////////////////
 
@@ -359,4 +359,9 @@ function checkUrl(e) {
    } else {
      window.location.href = this.location.href;
    }
+};
+
+function showSearch() {
+  $('#main_search').toggleClass('main-search-on');
+  $('.search-bar-bg-off').toggleClass('search-bar-bg-on');
 };

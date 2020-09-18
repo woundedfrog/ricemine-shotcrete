f6d659419aab134ded146dbfd5181d1374aea11b
Bern <ntjust26@gmail.com>
2020-09-18 15:02:14 +0900

    global ref list is updated

--- data/temp.rb
+++ data/temp.rb
@@ -4,13 +4,32 @@ require 'pry'
 
 
 
-def add_replace(unit, name_key, details)
-  binding.pry if name_key == 'moa'
-  unit['tiers'] = details['tier']
-  unit['date'] = details['date']
-  unit['notes'] = details['notes']
+def add_replace(unit, name_key, details, not_exist_yet = false)
+  # binding.pry if name_key == 'moa'
+  n_unit = {}
+  if not_exist_yet == true
 
-  [unit, name_key + ": updated"]
+  n_unit['idx'] = unit['idx']
+  n_unit['code'] = details
+  n_unit['en_name'] = name_key
+  n_unit['jp_name'] = ''
+  n_unit['kr_name'] = unit['name']
+  n_unit['image1'] = details
+  n_unit['image2'] = ''
+  n_unit['image3'] = ''
+  n_unit['tiers'] = ''
+  n_unit['notes'] = ''
+  n_unit['date'] = '2019-4-21'
+
+
+    p [n_unit, name_key + ": updated"]
+    else
+
+    unit['tiers'] = details['tier']
+    unit['date'] = details['date']
+    unit['notes'] = details['notes']
+    [unit, name_key + ": updated"]
+  end
 end
 
 
@@ -26,10 +45,32 @@ def check_confirm(unit, name, details)
         answer = false
       end
 
-    answer
+      answer
+  end
 end
 
+def add_units_without_ref
+  ## this is used to add units to the ref list that exists in the characterdatabase, but not in the ref list.
+  gldb = JSON.parse(File.read('childs/gl/CharacterDatabaseEn.json'))
+  reflist = JSON.parse(File.read('childs/gl/characterRefListGl.json'))
+
+  new_ref = []
+  gldb.each do |unit|
+    if unit['skins'].keys.any? {|l| l.include?("c")} || reflist.any? {|k| k['idx'] == unit['idx']}
+      next
+    else
+      skin = unit['skins'].sort_by {|k,v| k }[0][0]
+      name = unit['skins'].sort_by {|k,v| k }[0][1]
+      # binding.pry
+      new = add_replace(unit, name, skin, true)
+      new_ref << new[0] unless new_ref.include?(new[0])
+    end
+  end
+  binding.pry
+File.open('temp_ref.json', 'w') { |file| file.write(new_ref.to_json) }
+
 end
+
 
 def filter_data
   gldb = JSON.parse(File.read('childs/gl/CharacterDatabaseEn.json'))
@@ -46,7 +87,8 @@ def filter_data
     ref_name = unit['en_name'].downcase
 
 
-    x = gldb.find_index {|k,_| k['idx'] == unit['idx'] && k['grade'] == 5}
+
+    x = gldb.find_index {|k,_| k['idx'] == unit['idx'] && k['grade'] == 3 }# < change the 3 or 4 or 5 for star rating
      if unit['code'].include?('m') || (x.nil? && new_ref.include?(unit) == false)
 
       next
@@ -55,11 +97,12 @@ def filter_data
     # if (counter >= 0 && counter < 20)
       found = false
       ymldb.each do |name, details|
+        # next if details['stars'] != '3'
         matched = check_confirm(unit, name, details)
 
         if matched
           z = add_replace(unit, ref_name, details)
-          new_ref << z[0]
+          new_ref << z[0] unless new_ref.include?(z[0])
           updated << z[1]
           p "----" +z[1]+"----"
           puts
@@ -74,14 +117,19 @@ def filter_data
 
   end
 
+    binding.pry
   File.open('temp_ref.json', 'w') { |file| file.write(new_ref.to_json) }
   # File.open('childs/gl/characterRefListGl.json', 'w') { |file| file.write(new_ref.to_json) }
 
   binding.pry
 end
 
-
+#adds and replaced and updates data from the CharacterDatabaseEn with info from the YML database and updates the reflist
 filter_data
+
+#this updates the ref list with units that weren't found in the above method call.
+  add_units_without_ref
+
 File.join('data/', 'tooltips.yml')
 File.open('tooltips.yml', 'wb') { |f| f.write(@answers) }
 
